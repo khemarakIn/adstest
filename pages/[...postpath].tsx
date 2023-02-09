43762e797b15b3db4c236030493bb0ba4184e576
In Khemarak <inkhemarak63@gmail.com>
2023-02-09 10:51:09 +0700

    check title11

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -82,7 +82,7 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content={"):"}/>
+				<meta property="og:title" content={"."} key="og-title"/>
 				<link rel="canonical" href={` `} />
 				<meta property="og:description" content={" "} />
 				<meta property="og:url" content={` `} />
